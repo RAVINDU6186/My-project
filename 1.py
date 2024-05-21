47f72e91b83e416982c09085d7c4e78e68a97aa0
RAVINDU6186 <148838331+RAVINDU6186@users.noreply.github.com>
2024-05-21 23:33:35 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,5 +1,9 @@
-def counter(limit=10):
-    count = 0
+def counter(limit=10, reset=False):
+    if reset:
+        count = 0
+    else:
+        count = 0 if not hasattr(counter, 'count') else counter.count
     while count < limit:
         count += 1
         print(count)
+    counter.count = count
